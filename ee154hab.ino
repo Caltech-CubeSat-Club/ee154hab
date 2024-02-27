e5af7a3ba7b27350f2eb9f4c61a36f8b576f1923
KPulungan <51138958+KPulungan@users.noreply.github.com>
2024-02-27 03:27:29 -0800

    Commenting for heading

--- ee154hab.ino
+++ ee154hab.ino
@@ -13,7 +13,7 @@
 
   CSV format:
   Timestamp (ms), Sample Count, Date (if available), Latitude, Longitude, Ext. Pressure (Pa), Ext. Altitude (m), Humidity (%), Int. Temperature (°C), Ext. Temperature (°C), Rot X (°), Rot Y (°), Compass Heading (°), Rot X Rate (°/s), Rot Y Rate (°/s), Rot Z Rate (°/s), Accel X (m/s^2), Accel Y (m/s^2), Accel Z (m/s^2), Battery Current (A), Battery Temperature (°C)
-
+  Heading: East is negative, West is positive
 */
 
 #include <Wire.h>
